4cae16bdca93bbc7ff0e6d300e521968cc280a20
Gary Tsai <gary.m.tsai@gmail.com>
2013-11-19 12:28:59 -0800

    created gemfile and rspec folder

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'pg'
+gem 'jquery-rails'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do 
+	gem 'capybara'
+	gem 'selenium-webdriver'
+	gem 'shoulda-matchers'
+	gem 'simplecov'
+	gem 'database_cleaner'
+end
+
+group :development, :test do 
+	gem 'rspec-rails'
+	gem 'factory_girl_rails', '~> 4.0'
+	gem 'faker'
+	gem 'dotenv'
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxMenuPrimo::Application.config.secret_token = '0f807df0fd2da5b12bfc6b79ce8ebb93fc0982ade4e67a7e388d3a8cc05edf2e8b5d8e3a08b48911dbd537b804deffe43ec80ba8a6986b70c788caf6c51e3aed'
